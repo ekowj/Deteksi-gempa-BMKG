03d8224611239e811de88f43487e5d2224521816
ekowj <eko.wijaya03@gmail.com>
2022-08-10 09:36:16 +0700

    Fungsi ekstrasi_data

---
+++ Main.py
@@ -0,0 +1,18 @@
+"""
+Aplikasi Deteksi Gempa BMKG
+"""
+
+
+def ekstrasi_data():
+    pass
+
+
+def tampilkan_data(result):
+    pass
+
+
+if __name__== '__main__':
+    print('Aplikasi Utama')
+    result = ekstrasi_data()
+    tampilkan_data(result)
+    
